055de130778bfd7b86305802b65e7c0897d00fd0
David-Raffo <marconidavid@hotmail.es>
2026-05-25 13:09:28 +0200

    fix: new mail

--- src/content/social.ts
+++ src/content/social.ts
@@ -1,5 +1,5 @@
 export const social = [
-  { url: "mailto:marconidavid@hotmail.es", name: "mail" },
+  { url: "mailto:david@raffo.dev", name: "mail" },
   { url: "https://github.com/David-Raffo", name: "github" },
   { url: "https://www.linkedin.com/in/david-raf-loz/", name: "linkedin" },
   //{ url: "https://x.com/DavidHckh", name: "x" },
